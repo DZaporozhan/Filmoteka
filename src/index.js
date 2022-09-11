8528846c5b677d1cf134033973cb1dae0d67ea6c
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-09-11 15:47:52 +0300

    update refs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './js/markupModalMovie';
-import './js/refs';
-import './js/api-servise';
-import './js/cardTemplates';
 import './js/serchMovies';
 import './js/modalFooter';
 import './js/trendingMovies';
