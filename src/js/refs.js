6dfe361971180da298ee7efedd6a64750d4f6edd
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-09-11 12:17:56 +0300

    refs

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  mainList: document.querySelector('.movieList'),
+  warningOnSearch: document.querySelector('#js-input-error'),
+  modalFilmInfoRef: document.querySelector('.modal-film'),
+  footerButtomEl: document.querySelector('.footer__btn'),
+  backdropFooterEl: document.querySelector('.backdrop'),
+  spinnerEl: document.querySelector('.backdrop-spinner'),
+};
