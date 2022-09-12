e0df0805100054a53548fd15eac202f619fdbe10
Kate Vaskevich <kate.panasiuk@yahoo.com>
2022-09-12 14:06:39 +0300

    scroll-up-button

---
+++ src/js/scroll-up.js
@@ -0,0 +1,30 @@
+const target = document.querySelector('footer');
+const scrollUpBtn = document.querySelector('.scrollUpBtn');
+const rootEl = document.documentElement;
+
+// When scrolling on the website calling a function that calculates scrolled pixels
+window.onscroll = function () {
+  onScrollFunction();
+};
+
+// When scrolling more than 2500px class .showBtn either add or removed, which either reveal button or removes it
+function onScrollFunction() {
+  if (
+    document.body.scrollTop > 2500 ||
+    document.documentElement.scrollTop > 2500
+  ) {
+    scrollUpBtn.classList.add('showBtn');
+  } else {
+    scrollUpBtn.classList.remove('showBtn');
+  }
+}
+
+scrollUpBtn.addEventListener('click', scrollUpTop);
+
+// Function that scrolls the page up when clicked on the button
+function scrollUpTop() {
+  rootEl.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
