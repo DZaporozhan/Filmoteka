4613d9ab4e0d48fe891c1171f82966bffd1e9ef4
Krystyna Sharhorodska <rinjara@gmail.com>
2022-09-14 13:49:42 +0300

    Added back My Library btn, spinner fix

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,19 +1,19 @@
 import refs from './refs';
 
-window.addEventListener('load', onSpinnerEnabled);
-
-function onSpinnerEnabled() {
-  refs.spinnerEl.classList.add('visually-hidden');
-  setTimeout(() => {
-    refs.spinnerEl.remove();
-    window.removeEventListener('load', onSpinnerEnabled);
-  }, 600);
-}
+// window.addEventListener('load', onSpinnerEnabled);
 
 // function onSpinnerEnabled() {
-//   return refs.spinnerEl.classList.remove('visually-hidden');
+//   refs.spinnerEl.classList.add('visually-hidden');
+//   setTimeout(() => {
+//     refs.spinnerEl.remove();
+//     window.removeEventListener('load', onSpinnerEnabled);
+//   }, 600);
 // }
-// function onSpinnerDisabled() {
-//   return refs.spinnerEl.classList.add('visually-hidden');
-// }
-// export { onSpinnerEnabled, onSpinnerDisabled };
+
+function onSpinnerEnabled() {
+  return refs.spinnerEl.classList.remove('visually-hidden');
+}
+function onSpinnerDisabled() {
+  return refs.spinnerEl.classList.add('visually-hidden');
+}
+export { onSpinnerEnabled, onSpinnerDisabled };
