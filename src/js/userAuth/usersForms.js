66fa08aa8f75ef0dcc109e1eedb65a12ad780d27
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-09-15 02:24:14 +0300

    Clear code

--- src/js/userAuth/usersForms.js
+++ src/js/userAuth/usersForms.js
@@ -1,203 +1,163 @@
-// import refs from '../refs';
-// import {
-//     getAuth,
-//     onAuthStateChanged,
-//     createUserWithEmailAndPassword,
-//   deleteUser,
-//   signOut,
-//   updateProfile,
-//   updateEmail,
-//   updatePassword,
-//     signInWithEmailAndPassword,
-// } from 'firebase/auth';
-//   import { getDatabase, ref, set, remove } from 'firebase/database'
+import refs from '../refs';
+import {
+    getAuth,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { getDatabase, ref, set, remove } from 'firebase/database'
+  import { onSpinnerDisabled, onSpinnerEnabled } from '../spinner';
 
-// import { initializeApp } from 'firebase/app';
-// import { firebaseConfig } from "./firebaseConfig";
-// import { User } from "./authorization"
-// import { exit } from 'process';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from "./firebaseConfig";
 
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-// // const openAuth = document.querySelector('.open-auth')
-// // const backdropFooterEl = document.querySelector('.backdrop')
-// // const logOut = document.querySelector('.exit')
+refs.logOut.addEventListener('click', onLogOut)
+refs.toLogInTab.addEventListener('click',toLogInLink)
+refs.toSignUpTab.addEventListener('click', toSignUpLink)
+refs.toLogIn.addEventListener('click',toLogInLink)
+refs.toSignUp.addEventListener('click', toSignUpLink)
+refs.openAuth.addEventListener('click', openModalAuth)
+refs.backdropAuth.addEventListener('click', closeModalAuth)
+refs.signInForm.addEventListener('submit', submitAuth)
+refs.signUpForm.addEventListener('submit', submitRegister)
 
-// refs.logOut.addEventListener('click', onLogOut)
 
-// function onLogOut(){
-//   const user = auth.currentUser;
+document.addEventListener('keydown', closeModalAuth);
 
-//     signOut(auth).then(() => {
+function onLogOut(){
+  const user = auth.currentUser;
 
-//       location.reload();
-//     });
+    signOut(auth).then(() => {
 
-//     alert('You are logged out');
-// }
+      location.reload();
+    });
 
-// refs.openAuth.addEventListener('click', openModalAuth)
-// refs.backdropAuth.addEventListener('click', closeModalAuth)
-// document.addEventListener('keydown', closeModalAuth);
+    alert('You are logged out');
+}
 
-// function closeModalAuth(event) {
-//   if (event.code === 'Escape') {
-//     refs.backdropAuth.classList.add('is-hidden')
-//     refs.body.classList.remove('no-scroll');
-//   }
-// }
+function closeModalAuth(event) {
 
-// function openModalAuth() {
-//   refs.backdropAuth.classList.remove('is-hidden')
-//     refs.body.classList.add('no-scroll');
+  if (event.target.type === "button"|| event.target.type=== "email" ||event.target.type=== "password" || event.target.type=== "text"|| event.code === 'Enter') {
+    return
+  }
+  else if (event.target.type === "submit") {
+  }
+  else if (event.code === 'Escape') {
+    refs.backdropAuth.classList.add('is-hidden')
+    refs.body.classList.remove('no-scroll');
+  } else {
+    refs.backdropAuth.classList.add('is-hidden')
+    refs.body.classList.remove('no-scroll');
+  }
+  // console.log(event.target)
+}
 
-// }
-// // const library =document.querySelector('#library')
-// onAuthStateChanged(auth, user => {
-//   if (user) {
-//     refs.library.classList.remove('hidden-item');
-//     refs.openAuth.classList.add('hidden-item')
-//     refs.logOut.classList.remove('hidden-item')
+function openModalAuth() {
+  refs.backdropAuth.classList.remove('is-hidden')
+    refs.body.classList.add('no-scroll');
 
-//     // Включает My library
-//   } else {
-//     refs.library.classList.add('hidden-item');
-//     refs.openAuth.classList.remove('hidden-item')
-//     refs.logOut.classList.add('hidden-item')
-//     // User is signed out
-//     // ...
-//   }
-// });
+}
+onAuthStateChanged(auth, user => {
+  if (user) {
+    refs.library.classList.remove('hidden-item');
+    refs.openAuth.classList.add('hidden-item')
+    refs.logOut.classList.remove('hidden-item')
 
-// // const signIn = {
-// //     // openSignInBtn: document.querySelector('#login-btn'),
-// //   toLogIn: document.querySelector('#to-login'),
-// //     modalSignIn: document.querySelector('#auth'),
-// //     signInForm: document.querySelector('#auth-form')
-// // }
+    // Включает My library
+  } else {
+    refs.library.classList.add('hidden-item');
+    refs.openAuth.classList.remove('hidden-item')
+    refs.logOut.classList.add('hidden-item')
+    // User is signed out
+    // ...
+  }
+});
 
-// // signIn.openSignInBtn.addEventListener('click', openModal)
-// refs.signInForm.addEventListener('submit', submitAuth)
+function submitAuth(e){
+e.preventDefault()
+    const user = auth.currentUser;
+  const userData = {
+    email: document.getElementById("auth-email").value,
+    pass: document.getElementById("auth-password").value,
+  }
+function openModalAuth() {
+  refs.backdropAuth.classList.remove('is-hidden')
+    refs.body.classList.add('no-scroll');
 
-// function submitAuth(e){
-// e.preventDefault()
-//     const user = auth.currentUser;
-//     const userData = {
-//         email: document.getElementById("auth-email").value,
-//         pass: document.getElementById("auth-password").value,
+    if (!userData.email || !userData.pass) {
+      alert('Please enter your email and password');
+      return;
+  }
+  onSpinnerEnabled()
+signInWithEmailAndPassword(auth, userData.email, userData.pass)
+      .then(userCredential => {
+        const user = userCredential.user;
 
-//     }
+        refs.library.classList.remove('hidden-item');
+        onSpinnerDisabled()
+        // refs.backdropFooterEl.classList.add('is-hidden')
+        userData.email = ''
+        userData.pass= ''
 
-//     if (!userData.email || !userData.pass) {
-//       alert('Please enter your email and password');
-//       return;
-//     }
-// signInWithEmailAndPassword(auth, userData.email, userData.pass)
-//       .then(userCredential => {
-//         const user = userCredential.user;
+        alert(`User ${user.displayName} signed in`);
+        location.reload();
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage)
+      });
+      
+} 
 
-//         refs.library.classList.remove('hidden-item');
-//         // refs.backdropFooterEl.classList.add('is-hidden')
-//         userData.email.value = ''
-//         userData.pass.value = ''
+  
+}
+function submitRegister(e) {
+    e.preventDefault()
 
-//         alert(`User ${user.displayName} signed in`);
-//         location.reload();
-//       })
-//       .catch(error => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         alert(errorCode, errorMessage)
-//       });
+    updateProfile(auth.currentUser, {
+      displayName: `${userData.name}`,
+    });
 
-// // const user = new User();
 
-// //     user.logIn(userData);
-// }
+    onSpinnerEnabled()
+    createUserWithEmailAndPassword(auth, userData.email, userData.pass)
+      .then(userCredential => {
+        const user = userCredential.user;
 
-// // function openModal() {
-// //     // signIn.modalSignIn.classList.toggle('is-hidden')
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage);
+      });
 
-// // }
 
-// // const signUp = {
-// //     // openSignUpBtn: document.querySelector('#register-btn'),
-// //   toSignUp : document.querySelector('#to-signup'),
-// //     modalSignUp: document.querySelector('#reg'),
-// //     signUpForm: document.querySelector('#reg-form')
-// // }
+    alert(`User ${userData.name} created`);
+    onSpinnerDisabled()
+    signOut(auth).then(() => {
+      refs.library.classList.add('hidden-item');
+    });
+    userData.name = ''
+    userData.pass = ''
+    userData.email = ''
+    location.reload();
+  }
 
-// refs.signUpForm.addEventListener('submit', submitRegister)
-// // signUp.openSignUpBtn.addEventListener('click', openModalReg)
+function toLogInLink(e) {
+  e.preventDefault()
+refs.modalSignUp.classList.add('hidden-item')
+refs.modalSignIn.classList.remove('hidden-item')
+}
 
-// function submitRegister(e) {
-//     e.preventDefault()
-//     // const email = document.getElementById("register-email").value;
-//     // const password = document.getElementById("register-password").value;
-
-//     const userData = {
-//         name: document.getElementById('name').value,
-//         email: document.getElementById("register-email").value,
-//         pass: document.getElementById("register-password").value,
-
-//     }
-
-// createUserWithEmailAndPassword(auth, userData.email, userData.pass)
-//       .then(userCredential => {
-//         const user = userCredential.user;
-
-//         set(ref(db, 'users/' + user.uid + '/auth/'), userData);
-
-//         updateProfile(auth.currentUser, {
-//           displayName: `${userData.name}`,
-//         });
-
-//         alert(`User ${userData.name} created`);
-
-//         signOut(auth).then(() => {
-//           refs.library.classList.add('hidden-item');
-//         });
-//           userData.name.value = ''
-//         userData.pass.value = ''
-//           userData.email.value = ''
-//               location.reload();
-
-//       })
-//   .catch(error => {
-//                 const errorCode = error.code;
-//         const errorMessage = error.message;
-//         alert(errorCode, errorMessage);
-//       });
-
-// // const user = new User(userData);
-
-// //     user.create();
-// //      e.currentTarget.reset();
-// }
-// // function openModalReg() {
-
-// //     // signUp.modalSignUp.classList.toggle('is-hidden')
-// // }
-
-// //////////////////////////////////////
-
-// refs.toLogInTab.addEventListener('click',toLogInLink)
-// refs.toSignUpTab.addEventListener('click', toSignUpLink)
-// refs.toLogIn.addEventListener('click',toLogInLink)
-// refs.toSignUp.addEventListener('click', toSignUpLink)
-
-// function toLogInLink(e) {
-//   e.preventDefault()
-// refs.modalSignUp.classList.add('hidden-item')
-
-// refs.modalSignIn.classList.remove('hidden-item')
-// }
-
-// function toSignUpLink(e) {
-//   e.preventDefault()
-// refs.modalSignUp.classList.remove('hidden-item')
-
-// refs.modalSignIn.classList.add('hidden-item')
-// }
+function toSignUpLink(e) {
+  e.preventDefault()
+refs.modalSignUp.classList.remove('hidden-item')
+refs.modalSignIn.classList.add('hidden-item')
+}
